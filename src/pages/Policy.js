22467eca2375f25d07dbbef0320dd6b1b30b452d
Brooke Love <savienloveb@gmail.com>
2022-12-09 14:28:43 -0800

    changing background color on policy

--- src/pages/Policy.js
+++ src/pages/Policy.js
@@ -1,7 +1,8 @@
+import "../assets/css/pages/policy.css"
 export default function Policy() {
   return (
-    <>
+    <div className="policyComponent">
       <h1 className="galleryTitle">Policy</h1>
-    </>
+    </div>
   );
 }
